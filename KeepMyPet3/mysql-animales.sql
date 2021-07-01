f9da77abd38d72d5077fce3f044b66896c81af46
Bianca <dimodenabianca@gmail.com>
2021-07-01 23:16:48 +0200

    sql

--- KeepMyPet3/mysql-animales.sql
+++ KeepMyPet3/mysql-animales.sql
@@ -1,1 +1,88 @@
-SELECT * FROM jdbc.empleado;
+-- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+--
+-- Host: localhost    Database: keepmypet
+-- ------------------------------------------------------
+-- Server version	8.0.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `animales`
+--
+
+DROP TABLE IF EXISTS `animales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `animales` (
+  `AnimalNombre` varchar(45) NOT NULL,
+  `AnimalTipo` int NOT NULL,
+  `AnimalEdad` varchar(45) NOT NULL,
+  `AnimalId` int NOT NULL AUTO_INCREMENT,
+  `AnimalRaza` varchar(45) DEFAULT NULL,
+  `AnimalSexo` varchar(45) NOT NULL,
+  `AnimalAlta` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `AnimalPath` varchar(300) NOT NULL,
+  PRIMARY KEY (`AnimalId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `animales`
+--
+
+LOCK TABLES `animales` WRITE;
+/*!40000 ALTER TABLE `animales` DISABLE KEYS */;
+/*!40000 ALTER TABLE `animales` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `UsuId` int NOT NULL AUTO_INCREMENT,
+  `UsuNombre` varchar(45) NOT NULL,
+  `UsuApellido` varchar(45) NOT NULL,
+  `UsuNacimiento` date NOT NULL,
+  `UsuAlta` datetime DEFAULT CURRENT_TIMESTAMP,
+  `UsuMail` varchar(45) NOT NULL,
+  `UsuPass` varchar(45) NOT NULL,
+  `UsuRol` varchar(45) DEFAULT NULL,
+  `UsuCiudad` varchar(45) NOT NULL,
+  `UsuDNI` varchar(45) NOT NULL,
+  `UsuTelf` int NOT NULL,
+  PRIMARY KEY (`UsuId`,`UsuMail`,`UsuDNI`,`UsuTelf`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-01 23:16:29
